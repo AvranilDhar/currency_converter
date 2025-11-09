990476b4f12a29d7eb5b373a26b6f1e6666030b3
Avranil Dhar <179360557+AvranilDhar@users.noreply.github.com>
2025-11-09 21:17:26 +0530

    Enhance App layout and add custom fonts for improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen w-screen flex justify-center flex-col ' style={{backgroundColor : "#163300"}}>
+      <section className='text-center w-full'>
+        <p className='font-wiseSans w-full h-full text-8xl font-bold ' style={{color : "#9ee86f"}}>Currency Converter</p>
+        <p className='font-stackSans text-white text-wrap font-bold text-xl' style={{color : "#e8ebe6"}} >Real-time currency conversion built for travelers, traders, and everyday users</p>
+      </section>
+    </div>
   )
 }
 
